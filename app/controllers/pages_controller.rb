4ceded6fa47729a7f68d7b470aa1cb0465f49a41
Rohan Allison <rohanthewiz_at_gmail.com>
2016-10-18 16:15:15 -0500

    Fully implement paging

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,20 +6,28 @@ class PagesController < ApplicationController
 
   def search
     if page_params[:search_term]
+      pattern = page_params[:search_term].dup
+      if page_params[:language_filter].present?
+        @language_filter = page_params[:language_filter]
+        pattern << " language:#{page_params[:language_filter]}"
+      end
       octo_params = {}.tap do |octo|
         octo[:utf8] = page_params[:utf8] if page_params[:utf8].present?
         octo[:commit] = page_params[:commit] if page_params[:commit].present?
         octo[:page] = page_params[:page] if page_params[:page].present?
-
+        octo[:per_page] = 25
+        octo[:sort] = 'stars'
+        octo[:order] = 'desc'
       end
-      @results = get_client.search_repositories(page_params[:search_term], octo_params)
-      binding.pry
-      @search_term = params[:search_term]
+      @results = get_client.search_repositories(pattern, octo_params)
+      @search_term = page_params[:search_term]
+      puts @search_term
+      # &sort=stars&order=desc
     end
   end
 
   def page_params
-    params.permit(:search_term, :page, :utf8, :commit)
+    params.permit(:search_term, :language_filter, :page, :utf8, :commit)
   end
 
   private
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,6 @@
 module PagesHelper
+
+  def page_from_href(last_response_rel)
+    last_response_rel.href.match(/[&\?]page=(\d+)/)[1].to_i
+  end
 end
